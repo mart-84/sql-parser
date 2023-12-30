a5f9e2fb7ad4bbca36466f02d3c05ec9740aa5a6
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-30 22:09:35 +0100

    Ajout classe Token

---
+++ src/main/java/fr/martinbonnefoy/sql/parser/Token.java
@@ -0,0 +1,101 @@
+package fr.martinbonnefoy.sql.parser;
+
+import java.util.Objects;
+
+/**
+ * A lexical Token produced during the lexical analysis.<br>
+ * Represents a piece of text (value) assigned to a type.
+ */
+public class Token {
+
+	/**
+	 * The type of the Token
+	 */
+	private TokenType type;
+
+	/**
+	 * The value of the Token<br>
+	 * The default value is an empty string.
+	 */
+	private String value = "";
+
+	/**
+	 * Creates a new Token with the given type.<br>
+	 * The value of the token is set to an empty string.
+	 * 
+	 * @param type the type of the Token, must not be null
+	 */
+	public Token(TokenType type) {
+		this.type = type;
+	}
+
+	/**
+	 * Create a new Token with the given type and the given value.
+	 * 
+	 * @param type  the type of the Token, must not be null
+	 * @param value the value of the Token, must not be null, can be an empty string
+	 */
+	public Token(TokenType type, String value) {
+		this.type = type;
+		this.value = value;
+	}
+
+	/**
+	 * Returns the type of the Token.
+	 * 
+	 * @return the type
+	 */
+	public TokenType getType() {
+		return this.type;
+	}
+
+	/**
+	 * Sets the type of the Token.
+	 * 
+	 * @param type the type to set
+	 */
+	public void setType(TokenType type) {
+		this.type = type;
+	}
+
+	/**
+	 * Returns the value of the Token.
+	 * 
+	 * @return the value
+	 */
+	public String getValue() {
+		return this.value;
+	}
+
+	/**
+	 * Sets the value of the Token.
+	 * 
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.type, this.value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Token)) {
+			return false;
+		}
+		Token other = (Token) obj;
+		return this.type == other.type && Objects.equals(this.value, other.value);
+	}
+
+	@Override
+	public String toString() {
+		return "<" + this.type + ": '" + this.value + "'>";
+	}
+
+}
