23331e6718946a3e98cd7cce0843e232d637a2ff
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-30 22:08:54 +0100

    Création projet

---
+++ src/main/java/fr/martinbonnefoy/sql/parser/App.java
@@ -0,0 +1,12 @@
+package fr.martinbonnefoy.sql.parser;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello World");
+	}
+}
